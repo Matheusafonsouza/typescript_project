9f3dfad66629eba2fc61b61555f276c6212519a0
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-19 15:27:59 -0300

    chore: Created routes.ts and defined request/response types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 
+import { helloWorld } from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.status(200).json({ ok: true });
-});
+app.get('/', helloWorld);
 
 app.listen(3333);
